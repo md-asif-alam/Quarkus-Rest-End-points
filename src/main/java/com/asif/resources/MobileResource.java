ccfc25b84e81e959a6518366173da97716377ba8
Md Asif Alam <mdasifalam910@gmail.com>
2024-02-05 15:52:53 +0530

    update and delete request added

--- src/main/java/com/asif/resources/MobileResource.java
+++ src/main/java/com/asif/resources/MobileResource.java
@@ -6,6 +6,7 @@ import jakarta.ws.rs.core.Response;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Path("/mobile")
 public class MobileResource {
@@ -20,8 +21,37 @@ public class MobileResource {
 
     @POST
     @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
     public Response addNewMobile(String mobileName){
         mobileList.add(mobileName);
         return Response.ok(mobileName).build();
+    }
+
+    @PUT
+    @Path("/{oldmobilename}")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response updateMobile(@PathParam("oldmobilename") String oldMobileName , @QueryParam("newmobilename") String newMobileName){
+       mobileList= mobileList.stream().map(mobile->{
+            if(mobile.equals(oldMobileName)){
+                return newMobileName;
+            }else{
+                return mobile;
+            }
+        }).collect(Collectors.toList());
+        return Response.ok(mobileList).build();
+    }
+
+    @DELETE
+    @Path("/{mobileToDelete}")
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response deleteMobile(@PathParam("mobileToDelete") String deleteMobile){
+        boolean isRemoved = mobileList.remove(deleteMobile);
+        if(isRemoved){
+            return Response.ok(mobileList).build();
+        }else{
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
     }
 }
